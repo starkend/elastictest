b514d2ac0a0c0980dda3c8d0badfd533b53c3421
starkend <danstarken@gmail.com>
2019-09-14 12:11:40 -0500

    Initial commit

---
+++ src/main/java/com/starkend/elastictest/ElastictestApplication.java
@@ -0,0 +1,13 @@
+package com.starkend.elastictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastictestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElastictestApplication.class, args);
+    }
+
+}
