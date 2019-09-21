870bdaa23956808f0fdcee76a546955fd3280ce9
starkend <danstarken@gmail.com>
2019-09-21 12:29:22 -0500

    Add Elasticsearch High Level Client. Can't use Spring Data due to outdated Elasticsearch version and I want the latest Elasticsearch version, which is 7.3.2 as of now.

---
+++ src/main/java/com/starkend/elastictest/config/ElasticConfig.java
@@ -0,0 +1,26 @@
+package com.starkend.elastictest.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticConfig {
+
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Value("${elasticsearch.port}")
+    private int  elasticsearchPort;
+
+    @Bean
+    RestHighLevelClient restHighLevelClient() {
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(elasticsearchHost, elasticsearchPort)));
+
+        return restHighLevelClient;
+    }
+}
