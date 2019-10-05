2c2e5c04d3bfcff3436d2b00f6f76a3a3d06c059
starkend <danstarken@gmail.com>
2019-10-05 14:23:55 -0500

    Added Product Controller with getAllProducts Endpoint

--- src/main/java/com/starkend/elastictest/controller/ProductController.java
+++ src/main/java/com/starkend/elastictest/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.starkend.elastictest.controller;
 
 import com.starkend.elastictest.model.Product;
 import com.starkend.elastictest.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +14,19 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    private static Logger LOG = LoggerFactory.getLogger(ProductController.class);
+
     ProductRepository productRepository;
 
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-//    @GetMapping("/products")
-//    public ResponseEntity<List<Product>> getAllProducts() {
-////        productRepository.
-//    }
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> productList = productRepository.getAllProducts();
+
+        LOG.info(productList.toString());
+        return new ResponseEntity<>(productList, HttpStatus.OK);
+    }
 }
