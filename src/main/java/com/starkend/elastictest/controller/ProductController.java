4fedc19ad714f5417536836c0a2e70fe540b2493
starkend <danstarken@gmail.com>
2019-10-06 09:44:31 -0500

    Added Product findProductsByName endpoint to controller

--- src/main/java/com/starkend/elastictest/controller/ProductController.java
+++ src/main/java/com/starkend/elastictest/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,6 +26,14 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> productList = productRepository.getAllProducts();
+
+        LOG.info(productList.toString());
+        return new ResponseEntity<>(productList, HttpStatus.OK);
+    }
+
+    @GetMapping("/findProductsByName")
+    public ResponseEntity<List<Product>> getProductsByName(@RequestParam String name) {
+        List<Product> productList = productRepository.findByName(name);
 
         LOG.info(productList.toString());
         return new ResponseEntity<>(productList, HttpStatus.OK);
