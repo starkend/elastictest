ae9db16311013753bcfe9fb4ae482b47d1c89ebe
starkend <danstarken@gmail.com>
2019-10-20 12:36:04 -0500

    Code cleanup

--- src/main/java/com/starkend/elastictest/ElastictestApplication.java
+++ src/main/java/com/starkend/elastictest/ElastictestApplication.java
@@ -1,14 +1,11 @@
 package com.starkend.elastictest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.starkend.elastictest.service.DatakickService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import javax.xml.crypto.Data;
 
 @SpringBootApplication
 public class ElastictestApplication {
@@ -26,5 +23,4 @@ public class ElastictestApplication {
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
-
 }
