622b45bdb6799e1af2cdb82e03eabb18f2ccdeb8
starkend <danstarken@gmail.com>
2020-09-12 11:05:46 -0500

    Code cleanup

--- src/main/java/com/starkend/elastictest/service/ProductService.java
+++ src/main/java/com/starkend/elastictest/service/ProductService.java
@@ -1,51 +1,19 @@
 package com.starkend.elastictest.service;
 
-import com.starkend.elastictest.dto.ProductDto;
-//import com.starkend.elastictest.model.DKProduct;
 import com.starkend.elastictest.repository.ProductRepository;
-import com.starkend.elastictest.util.ProductUtils;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ProductService {
 
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-//    public DKProduct insertProduct(DKProduct DKProduct) {
-//        return productRepository.insertProduct(DKProduct);
-//    }
-//
-//    public List<DKProduct> findByName(String name) {
-//        return productRepository.findByName(name);
-//    }
-
     public boolean deleteById(String id) {
         return productRepository.deleteById(id);
     }
 
-//    public DKProduct findById(String id) {
-//        return productRepository.findById(id);
-//    }
-//
-//    public List<DKProduct> getAllProducts() {
-//        return productRepository.getAllProducts();
-//    }
-
-//    public List<ProductDto> getAllProductsAsDtos() {
-//        List<ProductDto> productDtos = new ArrayList<>();
-//        getAllProducts().forEach(product -> productDtos.add(ProductUtils.convertProductToProductDto(product)));
-//
-//        return productDtos;
-//    }
-
-//    public boolean bulkInsertProducts(List<DKProduct> insertDKProductList) {
-//        return productRepository.bulkInsertProducts(insertDKProductList);
-//    }
 }
